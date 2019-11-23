badede02ca5b62dbfeb0a2cfcbe66d1468eb0b1f
Paul Catalan <contact.pcatl@gmail.com>
2019-11-23 21:33:26 +0800

    Visual changes; Added go-to buttons

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+var mybutton = document.getElementById('to-top');
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = 'block';
+    } else {
+        mybutton.style.display = 'none';
+    }
+}
