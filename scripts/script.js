b12e6f913e5f0f7f6118acd9d531ce20cb8f22ff
Paul Catalan <contact.pcatl@gmail.com>
2019-11-24 10:12:18 +0800

    Color changes; Visual improvements

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,23 @@ function scrollFunction() {
         mybutton.style.display = 'none';
     }
 }
+
+var subtitles = [
+    'A Research and Development Engineer',
+    'Sometimes a computer repairperson..',
+    'Almost the typical programmer.',
+    'Technically a cat person..',
+    'I love reading.',
+    'My last name is a language!',
+    'I am creative.',
+    'Out of mana!',
+    'Bleep blop..'
+];
+
+var iter = 0;
+setInterval(function() {
+    console.log(iter);
+    document.getElementsByClassName('profession')[0].innerHTML = subtitles[iter];
+    ++iter;
+    iter %= subtitles.length;
+}, 2000);
