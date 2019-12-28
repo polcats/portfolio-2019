ff0607d6466784270ca1733da996d7ae4125461d
polcats <24451518+polcats@users.noreply.github.com>
2019-12-28 16:55:36 +0800

    Minor content changes. Updated style for mobile view.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var mybutton = document.getElementById('to-top');
+var mybutton = document.getElementById("to-top");
 
 window.onscroll = function() {
     scrollFunction();
@@ -6,30 +6,30 @@ window.onscroll = function() {
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = 'block';
+        mybutton.style.display = "block";
     } else {
-        mybutton.style.display = 'none';
+        mybutton.style.display = "none";
     }
 }
 
 var subtitles = [
-    'Sometimes a computer repairman.',
-    'Almost the typical programmer.',
-    'Technically a cat person..',
-    'Loves to read.',
+    "Sometimes a computer repairman.",
+    "Almost the typical programmer.",
+    "Technically a cat person..",
+    "Loves to read.",
     "Last name's a language!",
-    'Creative.',
-    'Out of mana!',
-    'Bleep blop..',
-    'A Research and Development Engineer'
+    "Creative.",
+    "Out of mana!",
+    "Bleep blop..",
+    "Research and Development Engineer"
 ];
 
 var iter = 0;
 setInterval(function() {
-    var subtitle = document.createElement('h1');
-    subtitle.setAttribute('class', 'subtitle profession fade-in');
+    var subtitle = document.createElement("h1");
+    subtitle.setAttribute("class", "subtitle profession fade-in");
     subtitle.innerHTML = subtitles[iter];
-    document.getElementsByClassName('profession')[0].replaceWith(subtitle);
+    document.getElementsByClassName("profession")[0].replaceWith(subtitle);
     ++iter;
     iter %= subtitles.length;
 }, 2000);
